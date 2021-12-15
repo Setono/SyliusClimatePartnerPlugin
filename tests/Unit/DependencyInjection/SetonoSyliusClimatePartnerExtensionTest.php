4079b071aaf56605e50c123ee8433755b97a8e97
Joachim Løvgaard <joachim@loevgaard.dk>
2021-12-15 12:31:01 +0100

    Add extension test

--- tests/Unit/DependencyInjection/SetonoSyliusClimatePartnerExtensionTest.php
+++ tests/Unit/DependencyInjection/SetonoSyliusClimatePartnerExtensionTest.php
@@ -6,6 +6,12 @@ namespace Tests\Setono\SyliusClimatePartnerPlugin\Unit\DependencyInjection;
 
 use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
 use Setono\SyliusClimatePartnerPlugin\DependencyInjection\SetonoSyliusClimatePartnerExtension;
+use Setono\SyliusClimatePartnerPlugin\Form\Type\ChannelClimateFeeType;
+use Setono\SyliusClimatePartnerPlugin\Model\ChannelClimateFee;
+use Setono\SyliusClimatePartnerPlugin\Repository\ChannelClimateFeeRepository;
+use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
+use Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
+use Sylius\Component\Resource\Factory\Factory;
 
 /**
  * See examples of tests and configuration options here: https://github.com/SymfonyTest/SymfonyDependencyInjectionTest
@@ -17,5 +23,26 @@ final class SetonoSyliusClimatePartnerExtensionTest extends AbstractExtensionTes
         return [
             new SetonoSyliusClimatePartnerExtension(),
         ];
+    }
+
+    /**
+     * @test
+     */
+    public function it_registers_resources(): void
+    {
+        $this->load();
+
+        $this->assertContainerBuilderHasParameter('sylius.resources', [
+            'setono_sylius_climate_partner.channel_climate_fee' => [
+                'driver' => SyliusResourceBundle::DRIVER_DOCTRINE_ORM,
+                'classes' => [
+                    'controller' => ResourceController::class,
+                    'factory' => Factory::class,
+                    'form' => ChannelClimateFeeType::class,
+                    'model' => ChannelClimateFee::class,
+                    'repository' => ChannelClimateFeeRepository::class,
+                ],
+            ],
+        ]);
     }
 }
